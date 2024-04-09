e8e8f90f006783ff049de922b9b2bee79e0d9df7
Syamkarni Uppalapati <101443460+syamkarni@users.noreply.github.com>
2024-04-10 01:43:09 +0530

    personal data html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form autocomplete="on">
+      <div className="section-container" id="Personal_info">
+        <div className="section-main">
+          <label for="fname">First Name:</label><br></br>
+          <input type="text" id="fname" name="fname" required/><br></br>
+          <label for="sname">Second Name:</label><br></br>
+          <input type="text" id="sname" name="sname" required/><br></br>
+          <label for="email">Email:</label><br></br>
+          <input type="email" id="email" name="email" required/><br></br>
+          <label for="phone">Phone:</label><br></br>
+          <input type="tel" id="phone" name="phone" required/><br></br>
+          <label for="plink">Personal link:</label><br></br>
+          <input type="url" id="plink" name="plink" required/><br></br>
+          <div className="address-container"> {/*Add if statements and then add options for the state and the country*/}
+            <label for="address">Address:</label><br></br>
+            <input type="text" id="address" name="address" required/><br></br>
+            <label for="city">City:</label><br></br>
+            <input type="text" id="city" name="city" required/><br></br>
+            <label for="zip">Zip:</label><br></br>
+            <input type="text" id="zip" name="zip" required/><br></br>
+            <label for="country">Country:</label><br></br>
+            <input type="text" id="country" name="country" required/><br></br>
+          </div>
+          <a href="#">Next</a>
+        </div>
+      </div>
+      </form>
     </div>
   );
 }
